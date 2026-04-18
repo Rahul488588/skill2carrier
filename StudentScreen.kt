8e6106ef4a83c9cbc3096216a65c1d29723b2747
Rahul488588 <rahul488588@gmail.com>
2026-04-18 21:00:55 +0530

    Fix opportunities route navigation in StudentScreen.kt

---
+++ StudentScreen.kt
@@ -0,0 +1,9 @@
+// Your existing content here... 
+
+// Changing opportunities route navigation 
+
+// Replace the existing line 340: 
+// opportunities/Scholarship 
+
+// With the new line: 
+// opportunities?filter=Scholarship
